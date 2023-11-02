c6ba2ed654440e832fa506009d3c80ca99406451
ethantx <149171034+ethantx@users.noreply.github.com>
2023-11-02 12:09:04 +0800

    Update main.go
    快捷配置方法不读取配置文件

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 
 func main() {
 	grpcx.Resolver.Register(etcd.New("127.0.0.1:2379"))
-	s := grpcx.Server.New()
+	s := grpcx.Server.New(&grpcx.GrpcServerConfig{
+		Address: ":850",//设置端口
+		Name: "test",//设置通道名称(不要与现有的重名 必须用英文名。系统会根据这个名进行调用的)
+		LogStdout:        true,//输出日志
+		AccessLogEnabled: true,//开启访问日志  快捷配置方法不读取配置文件
+		
+	})
 	codegrpc.Register(s)
 	s.Run()
 }
